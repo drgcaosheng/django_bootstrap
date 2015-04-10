b86d675ca84b3d8522b6e1855db5f58502b4b0a9
drgcaosheng <drgcaosheng@163.com>
2015-04-10 18:06:22 +0800

    反垃圾文件
    反垃圾文件

--- python_Chinese_rules/pyrules.py
+++ python_Chinese_rules/pyrules.py
@@ -4,24 +4,17 @@
 
 import os,sys,codecs,re
 f=open('Chinese_rules.cf','r')
-# re.search(r'header CN_SUBJECT',line):
-regex=re.compile(r'header CN_SUBJECT_\d{1,5}	Subject =~ /(.{1,50})/')
-alist=[]
+regex_subject=re.compile(r'header CN_SUBJECT_.{1,8}Subject =~ /(.{1,50})/')
+regex_number=re.compile(r'score CN_SUBJECT_.{1,8}	(.{1,5})')
 flines=f.readlines()
-a=1
+
 for line in flines:
-    if re.search(r'header CN_SUBJECT_',line):
-        print line+str(a),
-        a+=1
-
-# for line in flines:
-#     alist.append(regex.findall(line),)
-# a=1
-# for i in alist:
-#     if len(i)>0:
-#         print a
-#         print i[0]
-#         a+=1
-
+    # if re.search(r'header CN_SUBJECT_',line):
+    #     print "subject\t"+regex_subject.findall(line)[0]
+    # if re.search(r'score CN_SUBJECT_',line):
+    #     print regex_number.findall(line)[0]
+    print regex_subject.findall(line)
 
 f.close()
+
+
