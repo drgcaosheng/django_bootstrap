745c88bde7495ea461e0c0f966946d889abed054
drgcaosheng <drgcaosheng@163.com>
2015-04-20 23:59:29 +0800

    3
    3

--- python_Chinese_rules/pyrules.py
+++ python_Chinese_rules/pyrules.py
@@ -211,18 +211,128 @@ class createRules:
 class createRules2:
     def __init__(self):
         self.filename="jl_wtchinese_rules.txt"
+        self.rulease_list=[]
+        self.subjectDict={}
+        self.bodyDict={}
+        self.subjectlist=[]
+        self.bodylist=[]
+
+    #获取传递过来的参数,或者也叫整理参数
     def inputRules(self):
         typeRules=raw_input("Please input Type <body/subject> :")
         keyWordRules=raw_input("Please input keyWord: ")
         numberRules=raw_input("Please input number: ")
-        if typeRules.lower()!="body" or typeRules.lower()!="subject":
-            print 'error_type'
-        print typeRules,keyWordRules,numberRules
+        if typeRules.lower()=="body" or typeRules.lower()=="subject":
+            print typeRules,keyWordRules,numberRules
+        else:
+            return "type_error"
 
+    #读取文件.返回文件列表
+    def readTq(self):
+        f=open(self.filename,'r')
+        flines=f.readlines()
+        for line in flines:
+            self.rulease_list.append(line.split('\t'))
+        return self.rulease_list
+
+    #整理文件列表,返回两个字典
+    def rulesDict(self):
+        for rulease_one in self.rulease_list:
+            # print rulease_one
+            if rulease_one[0].lower()=='subject':
+                self.subjectDict[rulease_one[0]+'_'+rulease_one[1]]=rulease_one
+            elif rulease_one[0].lower()=='body':
+                self.bodyDict[rulease_one[0]+'_'+rulease_one[1]]=rulease_one
+    #查询
+    def searchRules(self,typeRules,keyWord,numberRules):
+        try:
+            self.rulesKey=typeRules+'_'+keyWord
+            if typeRules.lower()=='body':
+                return self.bodyDict[self.rulesKey]
+            elif typeRules.lower()=='subject':
+                return self.subjectDict[self.rulesKey]
+        except KeyError:
+            return False
+        except Exception,e:
+            return e
+
+    #添加
+    def addRules(self,typeRules,keyWord,numberRules):
+        self.rulesKey=typeRules+'_'+keyWord
+        print 'OK'
+        search_key=self.searchRules(typeRules,keyWord,numberRules)
+        if isinstance(search_key,list):
+            print 'list'
+        else:
+            if typeRules.lower()=='body':
+                self.bodyDict[self.rulesKey]=[typeRules,keyWord,numberRules+'\r\n']
+            elif typeRules.lower()=='subject':
+                self.subjectDict[self.rulesKey]=[typeRules,keyWord,numberRules+'\r\n']
+            print 'add_ok'
+        print self.bodyDict,self.subjectDict
+
+    #更改
+    def updateRules(self,typeRules,keyWord,numberRules):
+        self.rulesKey=typeRules+'_'+keyWord
+        search_key=self.searchRules(typeRules,keyWord,numberRules)
+        if isinstance(search_key,list):
+            if typeRules.lower()=='body':
+                self.bodyDict[self.rulesKey]=[typeRules,keyWord,numberRules+'\r\n']
+                # print self.bodyDict
+                return True
+            elif typeRules.lower()=='subject':
+                self.subjectDict[self.rulesKey]=[typeRules,keyWord,numberRules+'\r\n']
+                # print self.subjectDict
+                return True
+        else:
+            return False
+
+    #删除
+    def delRules(self,typeRules,keyWord,numberRules):
+        try:
+            self.rulesKey=typeRules+'_'+keyWord
+            if typeRules.lower()=='body':
+                del self.bodyDict[self.rulesKey]
+            elif typeRules.lower()=='subject':
+                del self.subjectDict[self.rulesKey]
+        except KeyError:
+            return False
+        except Exception,e:
+            return e
+
+    #格式化字典.
+    def readDict(self):
+        for k,v in self.subjectDict.items():
+            self.subjectlist.append(v[0]+'\t'+v[1]+'\t'+v[2])
+        for k,v in self.bodyDict.items():
+            self.bodylist.append(v[0]+'\t'+v[1]+'\t'+v[2])
+        self.bodyrules=''
+        for subjectone in self.subjectlist:
+            self.bodyrules+=subjectone
+        for bodyone in self.bodylist:
+            self.bodyrules+=bodyone
+        self.writeFile(self.bodyrules)
+
+    #写入文件
+    def writeFile(self,argv):
+        try:
+            create_Rules=open(self.filename,'w')
+            create_Rules.write(argv)
+            create_Rules.close()
+        except Exception,e:
+            print e
 
 if __name__=="__main__":
     cr=createRules2()
-    cr.inputRules()
+    cr.readTq()
+    cr.rulesDict()
+    # print cr.searchRules('subject','优惠','1.2')
+    # print cr.addRules('subject','优惠优惠优惠优惠','1.2222')
+    # cr.updateRules('subject','优惠','333')
+    # cr.delRules('subject','优惠','333')
+    # print cr.subjectDict
+    cr.readDict()
+    # cr.inputRules()
     # print cr.searchRules(u'body',u'有限公司',u'2')
     # print cr.addRules(u'body',u'有限公司',u'2')
 
