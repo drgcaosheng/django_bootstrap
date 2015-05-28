27225ae34b6c8aa6cda0ce25de63fd3289c138e1
drgcaosheng <drgcaosheng@163.com>
2015-05-28 16:29:57 +0800

    上传网络基础编程
    上传网络基础编程

---
+++ hello/app/views.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+from forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname':'CaoSheng'}
+    posts = [ # fake array of posts
+    {
+        'author': { 'nickname': 'John' },
+        'body': 'Beautiful day in Portland!'
+    },
+    {
+        'author': { 'nickname': 'Susan' },
+        'body': 'The Avengers movie was so cool!'
+    }
+    ]
+    return render_template('index.html',title='Home',user=user,posts=posts)
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sign In',form=form)
